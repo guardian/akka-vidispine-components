f34e290e3b15d87cff6703e23a1bf4e8bd599486
Andy Gallagher <andy.gallagher@theguardian.com>
2020-11-20 09:22:30 +0000

    WIP debugging stream failure if item shapes are not found

--- src/main/scala/com/gu/vidispineakka/streamcomponents/VSGetItem.scala
+++ src/main/scala/com/gu/vidispineakka/streamcomponents/VSGetItem.scala
@@ -3,15 +3,39 @@ package com.gu.vidispineakka.streamcomponents
 import akka.stream.{Attributes, FlowShape, Inlet, Materializer, Outlet}
 import akka.stream.stage.{AbstractInHandler, AbstractOutHandler, GraphStage, GraphStageLogic}
 import org.slf4j.LoggerFactory
-import com.gu.vidispineakka.vidispine.{VSCommunicator, VSFile, VSLazyItem}
+import com.gu.vidispineakka.vidispine.{VSCommunicator, VSFile, VSLazyItem, VSShape}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
-class VSGetItem(fieldList:Seq[String])(implicit comm:VSCommunicator, mat:Materializer, ec:ExecutionContext) extends GraphStage[FlowShape[VSFile, (VSFile, Option[VSLazyItem])]] {
+class NotFoundError(message:String) extends Throwable {
+  override def getMessage: String = message
+}
+
+class VSGetItem(fieldList:Seq[String], includeShapes:Boolean = false)(implicit comm:VSCommunicator, mat:Materializer, ec:ExecutionContext) extends GraphStage[FlowShape[VSFile, (VSFile, Option[VSLazyItem])]] {
   private final val in:Inlet[VSFile] = Inlet.create("VSGetItem.in")
   private final val out:Outlet[(VSFile, Option[VSLazyItem])] = Outlet.create("VSGetItem.out")
+  private val logger = LoggerFactory.getLogger(getClass)
 
   override def shape = FlowShape.of(in, out)
+
+  def loadVSShapeData(itemId:String) = {
+    comm.request(VSCommunicator.OperationType.GET,
+      s"/API/item/$itemId",
+      None,
+      Map("Accept"->"application/xml"),
+      Map("content"->"shape")
+    ).map({
+      case Left(httpErr)=>
+        logger.error(s"Could not get shape data for $itemId: ${httpErr.errorCode} ${httpErr.message}")
+        throw new NotFoundError("Could not get shape data")
+      case Right(xmlString)=>
+        val parsedContent = scala.xml.XML.loadString(xmlString)
+        val shapes = (parsedContent \ "shape").map(VSShape.fromXml)
+        val shapesMap = shapes.map(entry=>entry.tag->entry).toMap
+        if(shapesMap.isEmpty) None else Some(shapesMap)
+    })
+  }
 
   /**
     * create a new VSLazyItem. Included like this to make testing easier.
@@ -22,12 +46,11 @@ class VSGetItem(fieldList:Seq[String])(implicit comm:VSCommunicator, mat:Materia
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
     private val logger:org.slf4j.Logger = LoggerFactory.getLogger(getClass)
+    val completedCb = createAsyncCallback[(VSFile, Option[VSLazyItem])](itm=>push(out,(itm._1,itm._2)))
+    val failedCb = createAsyncCallback[Throwable](err=>failStage(err))
 
     setHandler(in, new AbstractInHandler {
       override def onPush(): Unit = {
-        val completedCb = createAsyncCallback[(VSFile, Option[VSLazyItem])](itm=>push(out,(itm._1,itm._2)))
-        val failedCb = createAsyncCallback[Throwable](err=>failStage(err))
-
         val elem = grab(in)
         logger.debug(s"Got incoming item $elem")
 
@@ -35,7 +58,13 @@ class VSGetItem(fieldList:Seq[String])(implicit comm:VSCommunicator, mat:Materia
           case Some(fileItemMembership)=>
             val item = makeItem(fileItemMembership.itemId)
 
-            item.getMoreMetadata(fieldList).map({
+            val itemWithShapesFut = if(includeShapes) {
+              loadVSShapeData(fileItemMembership.itemId).map(shapeinfo=>item.copy(shapes = shapeinfo))
+            } else {
+              Future(item)
+            }
+
+            itemWithShapesFut.flatMap(_.getMoreMetadata(fieldList).map({
               case Left(metadataError)=>
                 logger.error(s"Could not get metadata for item ${fileItemMembership.itemId}: $metadataError")
                 failedCb.invoke(new RuntimeException("Could not lookup metadata"))
@@ -43,10 +72,14 @@ class VSGetItem(fieldList:Seq[String])(implicit comm:VSCommunicator, mat:Materia
                 logger.debug(s"Looked up metadata for item ${updatedItem.itemId}")
                 completedCb.invoke((elem, Some(updatedItem)))
             }).recover({
+              case err:NotFoundError=>
+                logger.error(s"No item found for ${fileItemMembership.itemId}")
+                completedCb.invoke((elem, None))
               case err:Throwable=>
                 logger.error(s"Get metadata crashed for item ${fileItemMembership.itemId}")
                 failedCb.invoke(err)
-            })
+            }))
+
           case None=>
             logger.warn(s"Can't look up item metadata for file ${elem.vsid} as it is not a member of any item")
             completedCb.invoke((elem, None))
